af4abe7080e3e699a7e2dabd02d3ff330baf6510
Qua Vallery <qua.vallery@protonmail.com>
2020-09-20 03:14:28 +0000

    adding basics exercises

---
+++ Basics Exercises/Basics Exercises.rb
@@ -0,0 +1,33 @@
+#use a hash to store a list of movie titles with the year they came out. Then use puts command to make your program print out the year of each movie to the screen.
+movies = {jaws: 1975,
+  anchorman: 2004,
+  man_of_steel: 2013,
+  a_beautiful_mind: 2001,
+  the_evil_dead: 1981}
+  
+
+puts movies[:jaws]
+puts movies[:anchorman]
+puts movies[:man_of_steel]
+puts movies[:a_beautiful_mind]
+puts movies[:the_evil_dead]
+
+#use the dates from the previous example and store them in an array.Then make your program output the same thing as the previous exercise.
+dates = [1975, 2004, 2013, 2001, 1981]
+ 
+puts dates[0]
+puts dates[1]
+puts dates[2]
+puts dates[3]
+puts dates[4]
+
+#write a program that outputs the factorial of the numbers 5, 6, 7, 8.
+puts 5 * 4 * 3 * 2 * 1
+puts 6 * 5 * 4 * 3 * 2 * 1
+puts 7 * 6 * 5 * 4 * 3 * 2 * 1
+puts 8 * 7 * 6 * 5 * 4 * 3 * 2 * 1
+
+#write a program that calculates the square of 3 float numbers of your choosing and outputs the result to tthe screen.
+puts 5.2 ** 2
+puts 4.7 ** 2
+puts 8.4 ** 2
